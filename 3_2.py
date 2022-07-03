458ddd0250a9631baaae1cc9d9e538f6dae4e178
zhyun <like4m@naver.com>
2022-07-03 15:53:43 +0900

    숫자만 추출

---
+++ 3_2.py
@@ -0,0 +1,29 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+sInput=input()
+print(sInput)
+
+sRslt=""
+nRslt=0
+nAns=0
+for i in range(0, len(sInput)):
+    if(48 <= ord(sInput[i]) and ord(sInput[i]) <= 57):
+#        print(sInput[i])
+        sRslt+=(sInput[i])
+
+nRslt=int(sRslt)
+print(nRslt)
+
+for i in range(1,nRslt+1):
+    if(nRslt%i==0):
+        #print(i)
+        nAns+=1
+
+print(nAns)
+
+
+
+
+
+
