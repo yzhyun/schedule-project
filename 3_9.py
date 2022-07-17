9b00c83a6a660a35fc4646e5eff98ac9c9008bc7
zhyun <like4m@naver.com>
2022-07-17 16:25:32 +0900

    3.9

--- 3_9.py
+++ 3_9.py
@@ -3,8 +3,15 @@ sys.stdin=open("input.txt", "rt")
 
 n=int(input())
 arr=[list(map(int, input().split())) for _ in range(n)]
-arr2=[[0] *(n+2) for _ in range(n+2)]
+arr.insert(0, [0]*n)
+arr.append([0]*n)
 
+for li in arr:
+    li.insert(0,0)
+    li.append(0)
+
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 print("=====================================================")
 print(n)
 for li in arr:
@@ -12,9 +19,12 @@ for li in arr:
         print(i, end= ' ')
     print()
 print("=====================================================")
-for li in arr2:
-    for i in li:
-        print(i, end= ' ')
-    print()
-print("=====================================================")
+
+cnt=0
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if(all(arr[i][j]>arr[i+dx[k]][j+dy[k]] for k in range(4))):
+            cnt+=1
+print(cnt)
+
 
