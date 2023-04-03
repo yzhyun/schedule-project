79366e5f5eefca18525e770a9898dc3c745c8379
zhyun <like4m@naver.com>
2023-04-03 13:48:54 +0900

    solve the qiz

---
+++ quiz/answer/2_5.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n, k=map(int,input().split())
+print(n,k)
+ln=list(map(int, range(1,n+1)))
+lk=list(map(int, range(1,k+1)))
+print(ln)
+print(lk)
+
+
+lrslt=list(0 for i in range(n+k+1))
+print(lrslt)
+maxVal=0
+
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        lrslt[i+j]=lrslt[i+j]+1
+
+maxVal=max(lrslt)
+
+print("정답:", end= ' ')
+for i in range(len(lrslt)):
+    if(maxVal==lrslt[i]):
+        print(i, end=' ')
---
+++ quiz/answer/2_7.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+n=int(input())
+print(n)
+lrslt=list(map(int, range(1,n+1)))
+lb=[True]*(n+1)
+nRslt=0
+
+
+
+print("======================")
+
+
+for idx, bVal in enumerate(lb):
+    if(idx<2):
+        lb[idx]=False
+        continue
+    if(bVal):
+        for j in range(idx, (n//idx)+1):
+            lb[idx*j]=False
+
+print([i for i, val in enumerate(lb) if val])
+
+lval=[i for i, val in enumerate(lb) if val]
+print(len(lval))
