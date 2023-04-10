3719f1502ce87553ba9e3f58c3f528958907cf9c
zhyun <like4m@gmail.com>
2023-04-10 13:06:00 +0900

    new solve

---
+++ quiz/answer/3_5.py
@@ -0,0 +1,28 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+m,n=map(int,input().split())
+ln=list(map(int, input().split()))
+
+print(m,n)
+print(ln)
+
+lt=0
+rt=1
+tot=ln[0]
+cnt=0
+while True:
+    if(tot<n):
+        if(rt>=m):   break
+        else:
+            tot+=ln[rt]
+            rt+=1
+    elif(tot==n):
+        tot-=ln[lt]
+        lt+=1
+        cnt+=1
+    elif(tot>n):
+        tot-=ln[lt]
+        lt+=1
+
+print(cnt)
