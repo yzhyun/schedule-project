fb509165a23e985d6cbf457fabb358b2b360c605
zhyun <like4m@gmail.com>
2023-04-27 19:18:09 +0900

    solve

--- quiz/answer/4_3.py
+++ quiz/answer/4_3.py
@@ -1,31 +1,36 @@
 import sys
-sys.stdin=open("input.txt", "rt")
 
-n,m = map(int, input().split())
-print (n,m)
+sys.stdin = open("input.txt", "rt")
+
+n, m = map(int, input().split())
+print(n, m)
 
 ln = list(map(int, input().split()))
+#17
 
 print(ln)
 print(sum(ln))
-
 lt=ln[0]
 rt=sum(ln)
+res=0
 
-
-while lt <= rt:
-    mid=(lt+rt)//2
-    max = 0
-    cnt = 0
-    #체크로직 ( ln 의 val 값을 수용하느냐)
-    for i, val in enumerate(ln):
-        max += val
-        if max > mid :
+while lt<=rt:
+    mid = (lt+rt) // 2
+    cnt = 1
+    sum = 0
+    for x in ln :
+        if sum+x > mid :
             cnt += 1
-
-        if cnt >= 3 :
-            lt = mid+1
+            sum = x
         else :
-            rt = mid-1
+            sum += x
+    if cnt <= m:
+        rt = mid - 1
+        res = mid
+    else :
+        lt = mid + 1
 
-print(mid)
+
+print(res)
+
+
---
+++ quiz/answer/4_5.py
@@ -0,0 +1,41 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n=int(input())
+ln = [list(map(int, input().split())) for _ in range(n)]
+
+
+print(n)
+print(ln)
+
+ln.sort(key=lambda x: (x[1], x[0]))
+print(ln)
+
+cnt=1
+idx=0
+ed = ln[0][1]
+for i in range(1, n):
+
+    if  ln[i][0] >= ed:
+        ed = ln[i][1]
+        cnt += 1
+print(cnt)
+# while idx < n :
+#     a= ln[0][idx]
+#     print(a)
+#     idx+=1
+#     if nTrgt == a:
+#         nTrgt = ln[idx][1]
+#         cnt+=1
+
+
+# et=0
+# cnt=0
+# for s, e in ln :
+#     print(s,e)
+#     if s >= et :
+#         et=e
+#         cnt+=1
+#
+# print(cnt)
+
