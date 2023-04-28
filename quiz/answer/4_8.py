e924aaeb03ac2acbd9d23f86c3afedebfe035da7
zhyun <like4m@naver.com>
2023-04-28 11:21:23 +0900

    ne

---
+++ quiz/answer/4_8.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n,m = map(int, input().split())
+ln=list(map(int, input().split()))
+ln.sort()
+print(n, m)
+print(ln)
+
+cnt = 0
+while len(ln)>0:
+    if ln[0]+ln[len(ln)-1] > m :
+        ln.pop(len(ln)-1)
+        cnt+=1
+    else :
+        ln.pop(0)
+        ln.pop(len(ln)-1)
+        cnt+=1
+
+print(cnt)
+
+
+
